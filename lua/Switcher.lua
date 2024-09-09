644be67b0bfd419643d4bf1311ff3193a8f9c166
Mac-zyzds <Mac-zyzds>
2024-09-09 09:10:08 +0800

    fixing bug

--- lua/Switcher.lua
+++ lua/Switcher.lua
@@ -1,53 +1,54 @@
 local Switcher = {}
-
 Switcher.last_input_method = nil
 
 Switcher.get_current_input_method = function()
-	if not Switcher.last_input_method then
-		local handle = io.popen("im-select")
-		Switcher.last_input_method = handle:read("*a"):gsub("%s+", "")
-		handle:close()
-	end
-	return Switcher.last_input_method
+    if Switcher.last_input_method == "com.apple.keylayout.ABC" then
+        return Switcher.last_input_method
+    end
+
+    local handle = io.popen("im-select")
+    Switcher.last_input_method = handle:read("*a"):gsub("%s+", "")
+    handle:close()
+    return Switcher.last_input_method
 end
 
 Switcher.set_input_method = function(input_method)
-	os.execute("im-select " .. input_method)
+    os.execute("im-select " .. input_method)
 end
 
 Switcher.InsertEnter_callback = function()
-	if Switcher.last_input_method == nil then
-		Switcher.last_input_method = Switcher.get_current_input_method()
+    if Switcher.last_input_method == nil then
+        Switcher.last_input_method = Switcher.get_current_input_method()
         Switcher.set_input_method(Switcher.last_input_method)
-	end
+    end
 end
 
 Switcher.switch_to_en = function()
-	os.execute("im-select com.apple.keylayout.ABC")
+    os.execute("im-select com.apple.keylayout.ABC")
 end
-
+圣诞节
 Switcher.InsertLeave_callback = function()
-	Switcher.last_input_method = Switcher.get_current_input_method()
-	if Switcher.last_input_method ~= "com.apple.keylayout.ABC" then
-		Switcher.switch_to_en()
-	end
+    Switcher.last_input_method = Switcher.get_current_input_method()
+    if Switcher.last_input_method ~= "com.apple.keylayout.ABC" then
+        Switcher.switch_to_en()
+    end
 end
 
 Switcher.setup = function()
-	vim.api.nvim_create_autocmd("InsertEnter", {
-		callback = Switcher.InsertEnter_callback,
-	})
+    vim.api.nvim_create_autocmd("InsertEnter", {
+        callback = Switcher.InsertEnter_callback,
+    })
 
-	vim.api.nvim_create_autocmd("InsertLeave", {
-		callback = Switcher.InsertLeave_callback,
-	})
+    vim.api.nvim_create_autocmd("InsertLeave", {
+        callback = Switcher.InsertLeave_callback,
+    })
 end
 
 Switcher.helloworld = function()
     print("helloworld")
 end
 
-Switcher.print_cur_input_method = function ()
+Switcher.print_cur_input_method = function()
     print(Switcher.last_input_method)
 end
 
